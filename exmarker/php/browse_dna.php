aac7638f510314566e63e27dec16f085db571abc
ShangZhang <zhangs17@mails.tsinghua.edu.cn>
2020-07-23 04:04:07 +0800

    OK!

--- exmarker/php/browse_dna.php
+++ exmarker/php/browse_dna.php
@@ -1,12 +1,10 @@
 <?php
 ini_set('memory_limit', '1024M');
 $db = new PDO('mysql:host=localhost;dbname=bdlb1_1', 'root', '');
-$table = 'browse_RNA';
+$table='browse_DNA';
 
-$msgArray = array('code' => 0, 'data' => array(), 'message' => '参数接收错误，请关闭浏览器后重试。');
-$disease = isset($_POST['disease']) ? trim($_POST['disease']) : trim($_GET['disease']);
+$msgArray = array('code'=>0, 'data'=>array(), 'message'=>'参数接收错误，请关闭浏览器后重试。');
 $level = isset($_POST['level']) ? trim($_POST['level']) : trim($_GET['level']);
-$rna_type = isset($_POST['rna_type']) ? trim($_POST['rna_type']) : trim($_GET['rna_type']);
 
 header('content-type:application:json;charset=utf8');
 header('Access-Control-Allow-Origin:*');
@@ -14,19 +12,19 @@ header('Access-Control-Allow-Methods:POST');
 header('Access-Control-Allow-Headers:x-requested-with,content-type');
 
 
-function gtfinfor($disease, $level, $rna_type, $table)
-{
-        global $db;
-        $query = "select * from browse_RNA limit 1655";
+function gtfinfor($level){
+        global $db ;
+        $query = "select * from browse_DNA";
         $result = $db->query($query);
-        if (is_object($result)) {
+        if(is_object($result)){
                 $resultArray = $result->fetchAll();
                 return $resultArray;
-        } else {
+        }else{
                 echo "bu shi dui xiang";
                 echo $query;
                 echo gettype($result);
         }
 }
-$response = gtfinfor($disease, $level, $rna_type, $table);
+$response=gtfinfor($level);
 echo json_encode($response);
+?>
---
+++ exmarker/php/browse_oncokb.php
@@ -0,0 +1,29 @@
+<?php
+ini_set('memory_limit', '1024M');
+$db = new PDO('mysql:host=localhost;dbname=bdlb1_1', 'root', '');
+$table='oncokb_biomarker_drug_associations_FDA_approved_norm';
+
+$msgArray = array('code'=>0, 'data'=>array(), 'message'=>'参数接收错误，请关闭浏览器后重试。');
+// $level = isset($_POST['level']) ? trim($_POST['level']) : trim($_GET['level']);
+
+header('content-type:application:json;charset=utf8');
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Methods:POST');
+header('Access-Control-Allow-Headers:x-requested-with,content-type');
+
+
+function gtfinfor(){
+        global $db ;
+        $query = "select * from oncokb_biomarker_drug_associations_FDA_approved_norm";
+        $result = $db->query($query);
+        if(is_object($result)){
+                $resultArray = $result->fetchAll();
+                return $resultArray;
+        }else{
+                echo "bu shi dui xiang";
+                echo $query;
+                echo gettype($result);
+        }
+}
+$response=gtfinfor();
+echo json_encode($response);
